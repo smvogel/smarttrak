1623bf42d5e83e045bdedcda257604b784ed7519
smvogel <sam.vogel@hotmail.com>
2025-07-28 17:18:55 -0700

    fix implicit error line 43

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -40,7 +40,7 @@ export default function MainNavigation({ user }: NavigationProps) {
 
     const initials = name
         .split(' ')
-        .map(n => n[0])
+        .map((n: string) => n[0])
         .join('')
         .toUpperCase()
         .slice(0, 2);
